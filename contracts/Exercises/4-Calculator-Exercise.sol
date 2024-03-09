da31da9d4e3cd3bf9ff4dda14efc988a1e8ea984
ekankr2 <ekankr2@naver.com>
2024-03-09 16:36:48 +0900

    make: calculator

--- contracts/Exercises/4-Calculator-Exercise.sol
+++ contracts/Exercises/4-Calculator-Exercise.sol
@@ -10,4 +10,20 @@ pragma solidity ^0.8.0;
 contract Calculator {
     uint256 public result;
 
+    function add(uint256 num) public {
+        result += num;
+    }
+
+    function subtract(uint256 num) public {
+        result -= num;
+    }
+
+    function multiply(uint256 num) public {
+        result *= num;
+    }
+
+    function get() public view returns (uint256) {
+        return result;
+    }
+
 }
