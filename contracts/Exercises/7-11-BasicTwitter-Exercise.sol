1b6f67e792d8b523b84b1edaf43d09c8788b6668
ekankr2 <ekankr2@naver.com>
2024-03-10 20:05:13 +0900

    feat: struct

--- contracts/Exercises/7-11-BasicTwitter-Exercise.sol
+++ contracts/Exercises/7-11-BasicTwitter-Exercise.sol
@@ -2,14 +2,16 @@
 
 pragma solidity ^0.8.0;
 
-// 1️⃣ Create a Twitter Contract 
-// 2️⃣ Create a mapping between user and tweet 
-// 3️⃣ Add function to create a tweet and save it in mapping
-// 4️⃣ Create a function to get Tweet 
-// 5️⃣ Add array of tweets 
+// 1. Create a Twitter Contract ✅
+// 2. Create a mapping between user and tweet ✅
+// 3. Add function to create a tweet and save it in mapping ✅
+// 4. Create a function to get Tweet ✅
+// 5. Add array of tweets ✅
 
 contract Twitter {
-    mapping(address => string[] ) public tweets;
+
+    // add our code
+    mapping(address => string[]) public tweets;
 
     function createTweet(string memory _tweet) public {
         tweets[msg.sender].push(_tweet);
@@ -19,7 +21,7 @@ contract Twitter {
         return tweets[_owner][_i];
     }
 
-    function getAllTweets()  {
-
+    function getAllTweets(address _owner) public view returns (string[] memory){
+        return tweets[_owner];
     }
 }
